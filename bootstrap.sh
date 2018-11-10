c3b531725f333e46c36bdeb23387f085eaf65953
Alexey Gaziev <alex.gaziev@gmail.com>
2018-11-10 12:47:38 -0800

    Make script restartable

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,14 @@ fi
 
 playbook=$1
 
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
+if ! [ -x "$(command -v ansible-playbook)" ]; then
+    echo 'Installing ansible'
+    sudo apt-get update
+    sudo apt-get install -y software-properties-common
+    sudo apt-add-repository -y ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install -y ansible
+fi
+
+echo 'Running ansible playbook'
 ansible-playbook $playbook.yml
